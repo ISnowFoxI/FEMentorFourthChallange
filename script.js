699f8e2f86dbb40723d3ed48a911dbd8a6c9e9f1
SnowFox <65253170+ISnowFoxI@users.noreply.github.com>
2021-07-21 18:17:04 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const billAmount = document.getElementById("bill")
+
+const numberOfPeople = document.getElementById("numOfPeople")
+
+const tipPerc= document.getElementsByClassName("tipInputPercentage")
+
+const totalTipPerPerson = document.getElementById("totalTip")
+
+const totalPerPerson = document.getElementById("totalPerPerson")
+
+const sectionContainer = document.getElementById("sectionContainer")
+
+const reset = document.getElementById("reset")
+
+//variables
+let billAmountVar=0;
+let tipPercVar=5;
+let tipValue=0;
+let numberOfPeopleVar=1;
+let totalTipPerPersonVar=0;
+let totalPerPersonVar=0;
+
+const tips=[].slice.call(tipPerc)
+
+const getTipValue = (tipValue) => { 
+    tipPercVar=parseFloat(tipValue.target.value);
+
+}
+
+const getNumberOfPeople = (people)=> { 
+    numberOfPeopleVar=parseFloat(people.target.value);
+}
+
+
+const getBillAmount = (bill)=> { 
+    billAmountVar =parseFloat(bill.target.value);
+
+}
+
+const calculate = ()=> { 
+
+    tipValue = (billAmountVar*tipPercVar)/100;
+    totalTipPerPersonVar=tipValue/numberOfPeopleVar
+    totalTipPerPerson.value=`$${(totalTipPerPersonVar.toFixed(2))}` ;
+
+    totalPerPersonVar = (billAmountVar+tipValue)/numberOfPeopleVar;
+    console.log(totalPerPersonVar)
+    totalPerPerson.value=`$${(totalPerPersonVar.toFixed(2))}` ;
+
+}
+
+tips.forEach(tip => {
+    tip.addEventListener("input", tipValue=> getTipValue(tipValue))
+});
+
+numberOfPeople.addEventListener("input",people=> getNumberOfPeople(people))
+   
+billAmount.addEventListener("input", bill =>getBillAmount(bill))
+
+
+
+sectionContainer.addEventListener("input", calculate)
+
+reset.addEventListener("click", e=> { 
+    let billAmountVar=0;
+let tipPercVar=5;
+let tipValue=0;
+let numberOfPeopleVar=1;
+let totalTipPerPersonVar=0;
+let totalPerPersonVar=0;
+})
